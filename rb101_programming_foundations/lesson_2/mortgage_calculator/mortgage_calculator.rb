bd3680c632b58f5f6cf1a0798a804cbd671d2fde
April Giljahn <giljahn.40@gmail.com>
2023-01-21 20:26:05 -0500

    finished my mortgage calculator

---
+++ rb101_programming_foundations/lesson_2/mortgage_calculator/mortgage_calculator.rb
@@ -0,0 +1,108 @@
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_calculator.yml')
+
+def prompt(key)
+  message = messages(key)
+  puts("=> #{message}")
+end
+def messages(message)
+  MESSAGES[message]
+end
+
+def is_integer?(input)
+  input.to_i.to_s == input && input.to_i > 0
+end
+
+def is_float?(input)
+  input.to_f.to_s == input && input.to_f > 0.0
+end
+
+def valid_num?(input)
+  is_integer?(input) || is_float?(input)
+end
+
+def monthly_interest_rate(num)
+  (num / 100) / 12
+end
+
+def duration_months(num)
+  num * 12
+end
+
+
+def monthly_payment(num1, num2, num3)
+  (num1 / num2) * num3
+end
+
+
+prompt("welcome")
+
+loop do
+prompt("name")
+name = gets.chomp
+unless name.empty?
+  puts("Nice to meet you, #{name}!")
+  break
+else
+  prompt("valid_name")
+end
+end
+
+loop do
+loan_amount = ''
+loop do
+  prompt("loan_am")
+  loan_amount = gets.chomp
+  if valid_num?(loan_amount)
+    puts("OK, perfect! I have your loan amount as $#{loan_amount} USD")
+    break
+  else
+    prompt("valid_num")
+  end
+end
+apr = ''
+loop do
+prompt("enter_apr")
+apr = gets.chomp
+if valid_num?(apr)
+  puts("Alright, I have your APR as #{apr}%. Now on to the last thing...")
+  break
+else
+  prompt("valid_num")
+end
+end
+
+loan_duration = ''
+loop do
+prompt("loan_dur")
+loan_duration = gets.chomp
+if valid_num?(loan_duration)
+  puts("Perfect! your loan duration is #{loan_duration} years.")
+  break
+else 
+  prompt("valid_num")
+end
+end
+
+prompt("pert_info")
+
+monthly_interest_rate = monthly_interest_rate(apr.to_f)
+monthly_payment = loan_amount.to_i * (monthly_interest_rate/(1-(1+monthly_interest_rate)**(-duration_months(loan_duration.to_i))))
+monthly_interest_payment = monthly_payment - (loan_amount.to_f/duration_months(loan_duration.to_i))
+
+puts "Your loan duration is #{duration_months(loan_duration.to_i)} months."
+puts "Your monthly interest rate is #{monthly_interest_rate.round(5)}%"
+puts "Your monthly interest payment is $#{monthly_interest_payment.round(2)} USD."
+puts "Your monthly payment is $#{monthly_payment.round(2)} USD."
+
+prompt("again")
+answer = gets.chomp.to_i
+  
+if answer != 1
+  prompt("bye")
+  break
+else
+  prompt("another_round")
+end
+
+end
