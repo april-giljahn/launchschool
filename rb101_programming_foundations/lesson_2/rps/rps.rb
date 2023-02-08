1fbe40b564ab4e10cac6859d9870b7125800c325
April Giljahn <giljahn.40@gmail.com>
2023-02-08 06:47:15 -0500

    added ability to shorten words for rps game.

--- rb101_programming_foundations/lesson_2/rps/rps.rb
+++ rb101_programming_foundations/lesson_2/rps/rps.rb
@@ -29,19 +29,37 @@ def display_results(player, computer)
   end
 end
 
+def simple (word)
+  if word.start_with?("r")
+    word.gsub(word, "rock")
+  elsif word.start_with?("sp")
+    word.gsub(word, "spock")
+  elsif word.start_with?("p")
+    word.gsub(word, "paper")
+  elsif word.start_with?("s")
+    word.gsub(word, "scissors")
+  elsif word.start_with?("l")
+    word.gsub(word, "lizard")
+  else
+    puts "error"
+  end
+end
+
 loop do
   choice = ''
+  final_choice = ''
   loop do
     puts("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = gets.chomp
-    break if VALID_CHOICES.include?(choice)
+    final_choice = simple(choice)
+    break if VALID_CHOICES.include?(final_choice)
   end
 
   computer_choice = VALID_CHOICES.sample
 
-  display_results(choice, computer_choice)
+  display_results(simple(choice), computer_choice)
 
-  puts("You chose: #{choice}; computer chose #{computer_choice}")
+  puts("You chose: #{final_choice}; computer chose #{computer_choice}")
 
   prompt('again')
   again = gets.chomp
